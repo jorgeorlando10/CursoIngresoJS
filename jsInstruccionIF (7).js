46a9de2f4939abbb28b565f542f2f327fa9e73e1
jorgeorlando10 <jorgecjs94@gmail.com>
2017-02-09 22:09:01 -0300

    instruccionIF

---
+++ jsInstruccionIF (7).js
@@ -0,0 +1,19 @@
+function Mostrar()
+{
+//tomo la edad  
+
+	var edad;
+	var estadoCivil;
+	edad=document.getElementById("edad").value;
+	estadoCivil=document.getElementById("soltero").value;
+	if(edad<18 && >1)
+
+	{
+
+	}
+	edad=document.getElementById("edad").value;
+	estadoCivil=document.getElementById("casado").value;
+	if(edad>18)
+
+{
+}//FIN DE LA FUNCIÓN
---
+++ jsInstruccionIF (9).js
@@ -0,0 +1,7 @@
+function Mostrar()
+{
+	//Genero el número RANDOM entre 1 y 10 
+
+	var x = Math.floor((Math.random() * 10) + 1);
+ x
+}//FIN DE LA FUNCIÓN
