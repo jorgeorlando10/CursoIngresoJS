1b19c49a428f5c25c8b1c2e2de6e9f2b61bdab58
jorgeorlando10 <jorgecjs94@gmail.com>
2017-02-17 21:14:57 +0200

    ejercicio7

--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -5,13 +5,13 @@ function Mostrar()
 	var acumulador=0;
 	var respuesta='si';
 
-while(contador<)
-{
-	contador=contador+1;
-	numero=prompt("numeros");
-	numero=parseInt(numero);
-	acumulador=acumulador+numero;
-
+  while(contador8)
+  {
+ contador++;
+ 	numero=prompt("suma");
+ 	numero=parseInt(numero);
+ 	acumulador+=numero;
+ }
 
 document.getElementById('suma').value=acumulador;
 document.getElementById('promedio').value=acumulador/contador;
