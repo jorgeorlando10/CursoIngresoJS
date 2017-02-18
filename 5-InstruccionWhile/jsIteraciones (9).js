ffdc37d32a51687d68e22ebe15c5ff6872d18858
jorgeorlando10 <jorgecjs94@gmail.com>
2017-02-18 03:08:29 +0200

    ejercicio9

--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -3,16 +3,104 @@ function Mostrar()
 
 	var contador=0;
 	// declarar variables
-	
+	var numero;
 	var respuesta='si';
+	var maximo;
+	var minimo;
+	var nombre;
+	var nombreDelMayor;
+	var sexo;
+	var sexoMinimo
+	var notaMinima;
+	var nota;
+	var promedio;
+	var aprobado>4;
+	var bandera="nadie";
+	var nombreDelDiez;
+	var sexoDelDiez;
+	var edadDelDiez;
 
-	while(respuesta!='no')
+while(respuesta!='no')
 	{
-		
-	
+	edad=prompt("ingrese edad")
+	edad=parseInt(edad);
+
+while(edad>0||edad <100)
+	{	
+
+	edad=prompt("ingrese edad")
+	edad=parseInt(edad);
+	contador=++1;
+	nombre=prompt("por favor ingrese nombre")
+
+	sexo=prompt("ingrese sexo")
+	sexo=parseInt(sexo);
 	}
 
+while(sexo!="f" &&!="m")
+	{
+	sexo=prompt("ingrese sexo")
+	sexo=parseInt(sexo);
 
+while(nota>0 && edad <4)
+	{
+	 nota=promp("ingrese nota")	
+	 nota=parseInt(nota);
+	}	
+
+	//cantidad de personas aprobadas
+
+
+if (contador==1);
+
+	{
+
+	maximo=edad
+	minimo=edad
+	nombreDelMayor=nombre
+	nombreDelDiez=nombre
+
+
+	}
+else
+	{
+
+ if(edad>maximo)
+ {
+   	maximo=edad;
+	nombreDelMayor=nombre
+	}
+if  (edad<minimo)
+	{
+	minimo=edad;
+	nombreDelMayor=nombre
+	}
+
+if (contador==1)
+	{	
+		notaMinima=nota;
+		sexoMnimo=sexo
+	}
+else
+	}
+if(notaMinima=nota)
+	{
+		notaMinima=nota;
+		sexoMinimo=sexo;
+	}
+	if
+
+
+
+
+
+	respuesta=prompt("para salir tecle no")
+	}
+	}
+	}
+
+//la edad promedio de las mujeres
+//la cantidad de mujeres que aprobaron con nota par y son menores de edad
 
 
 }//FIN DE LA FUNCIÓN
