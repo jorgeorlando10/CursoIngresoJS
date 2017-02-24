7c9e45403180c8501c4c214923177da42a5bfe59
jorgeorlando10 <jorgecjs94@gmail.com>
2017-02-23 22:15:00 -0300

    ejercicios 1 y 2

--- 6-InstruccionFor/jsIteracionesFor (1).js
+++ 6-InstruccionFor/jsIteracionesFor (1).js
@@ -1,4 +1,98 @@
 function Mostrar()
 {
 
+/*for(var contador=0 ; contador;)
+	console.log(contador);
+	contador++;
+	if(contador==5)
+}
+	break;
+}
+*/
+var contador=0;
+var numero;
+var numeroRandom;
+var contador1=0;
+var contador2=0;
+var contador3=0;
+var contador4=0;
+var contador5=0;
+var contador6=0;
+var contador7=0;
+var contador8=0;
+var contador9=0;
+var contador10=0;
+
+for(;contador<100000 ; contador++)
+{
+	numeroRandom=Math.floor(Math.random()*(10-1)) + 1;
+	console.log(contador);
+	switch(numeroRandom)
+
+	{
+
+	case 1: 
+	contador1;
+	break;
+
+	case 2: 
+	contador2;
+	break;
+
+	case 3: 
+	contador3;
+	break;
+
+	case 4: 
+	contador4;
+	break;
+
+	case 5: 
+	contador5;
+	break;
+
+	case 6:
+	contador6;
+	break;
+
+	case 7:
+	if (contador7==7)
+	{
+	continue;
+	}
+
+	contador7++;
+	break
+
+	case 7:
+	contador7;
+    break;
+
+    case 8:
+	contador8;
+    break;
+
+	case 9:
+	contador9;
+    break;
+
+    case 10:
+	contador10;
+    break;
+}
+}
+console.log("la cantidad de uno es "+contador1/contador*100+"%")
+console.log("la cantidad de dos es "+contador2/contador*100+"%")
+console.log("la cantidad de tres es "+contador3/contador*100+"%")
+console.log("la cantidad de cuatro es "+contador4/contador*100+"%")
+console.log("la cantidad de cinco es "+contador5/contador*100+"%")
+console.log("la cantidad de seis es "+contador6/contador*100+"%")
+console.log("la cantidad de siete es "+contador7/contador*100+"%")
+console.log("la cantidad de ocho es "+contador8/contador*100+"%")
+console.log("la cantidad de nueve es "+contador9/contador*100+"%")
+console.log("la cantidad de diez es "+contador10/contador*100+"%")
+
+
+
+
 }
--- 6-InstruccionFor/jsIteracionesFor (2).js
+++ 6-InstruccionFor/jsIteracionesFor (2).js
@@ -1,5 +1,44 @@
 function Mostrar()
 {
+var contador=0
+var respuesta="si";
+var personaConMascota;
+var personaSinMascota;
+var tipoDeMascota;
+var edad;
+var mayorDeEdad;
 
 
-}
+ 	for(;contador>100 ;)
+ 	{
+
+	respuesta=prompt("tiene mascota");
+	while(respuesta=="si") &&respuesta!="no";
+	{
+	respuesta=prompt("ingrese si o no");
+
+	}
+	if (respuesta=="si")
+	{
+ 	contador=personaConMascota++;
+ 	tipoDeMascota=prompt("perro o gato");
+ 	while(tipoDeMascota!="perro" && tipoDeMascota!="gato");
+ 	{
+ 		tipoDeMascota=prompt("no  tiene mascota");
+	}
+
+	}
+	else 
+	{
+	contador=personasConMascota++;
+	if contador=0
+	{
+	mayorDeEdad=edad
+	}
+	else 
+	{
+edad>mayorDeEdad
+	}
+	}
+
+//necesito saber el nombre del primer gato que se  de una mujer mayor a 30 años y el gato no tenga mas de 5 años de edad .sexo edad,nombre del gato edade del gato 0<15
